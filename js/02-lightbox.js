461c72633355de17a523968f57bba413627a2a85
olhazamlynska <olya.sonechko@gmail.com>
2022-09-18 15:26:33 +0300

    Update task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryDivRef = document.querySelector(".gallery");
+
+const galleryImgItem = galleryItems
+  .map(({ preview, original, description }) => {
+    return /*html*/ `<li class = "gallery__li">
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+  })
+  .join("");
+
+galleryDivRef.innerHTML = galleryImgItem;
+
+const modalGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
